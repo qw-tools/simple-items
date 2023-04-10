35dc75fe2de885bf55a0104bd6fb218efd39937c
Viktor Persson <viktor.persson@arcsin.se>
2023-04-10 17:44:00 +0200

    Add application boilerplate.

---
+++ src/Site/config.ts
@@ -0,0 +1,18 @@
+export const pages = [
+  {
+    title: "Model Textures",
+    url: "https://tools.quake.world/model-textures/",
+  },
+  {
+    title: "Simple Items",
+    url: "https://tools.quake.world/simple-items/",
+  },
+  {
+    title: "HUD Numbers",
+    url: "https://tools.quake.world/hud-numbers/",
+  },
+  {
+    title: "Charset",
+    url: "https://tools.quake.world/charset/",
+  },
+];

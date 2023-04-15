541122457699b288e862d7c89ca9348436c8a985
Viktor Persson <viktor.persson@arcsin.se>
2023-04-15 13:57:08 +0200

    Reformat code.

--- src/features/SimpleItemsEditor/ItemSelection.vue
+++ src/features/SimpleItemsEditor/ItemSelection.vue
@@ -42,10 +42,7 @@ function onSelectInvert(): void {
       />
       Deselect all
     </button>
-    <button
-      class="flex items-center p-1.5 px-2"
-      @click="onSelectInvert"
-    >
+    <button class="flex items-center p-1.5 px-2" @click="onSelectInvert">
       <img class="w-5 h-5 mr-1" src="/assets/img/icons/alternate_48.png" />
       Invert selection
     </button>

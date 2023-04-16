38da135bfb2026a6072969beea51fd36ec237380
Viktor Persson <viktor.persson@arcsin.se>
2023-04-16 16:27:57 +0200

    Remove obsolete utils.

--- src/pkg/pixi.ts
+++
@@ -1,6 +0,0 @@
-import * as PIXI from "pixi.js";
-import { Point2D } from "./geometry";
-
-export function eventToPosition(e: PIXI.FederatedMouseEvent): Point2D {
-  return { x: e.global.x, y: e.global.y };
-}

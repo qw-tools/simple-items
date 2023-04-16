5924cca2146d1463dbaf38aaaffe2efe24c4f4ed
Viktor Persson <viktor.persson@arcsin.se>
2023-04-16 16:44:35 +0200

    Refactor deepCopy().

--- src/pkg/dataUtil.ts
+++ src/pkg/dataUtil.ts
@@ -1,38 +1,5 @@
-export function deepCopy(o) {
-  // Cas de base
-  if (
-    typeof o !== "object" ||
-    o === null ||
-    o instanceof RegExp ||
-    o instanceof Date
-  ) {
-    return o;
-  }
+import deepmerge from "deepmerge";
 
-  // Array
-  if (
-    Array.isArray(o) ||
-    o instanceof Int8Array ||
-    o instanceof Uint8Array ||
-    o instanceof Uint8ClampedArray ||
-    o instanceof Int16Array ||
-    o instanceof Uint16Array ||
-    o instanceof Int32Array ||
-    o instanceof Uint32Array ||
-    o instanceof Float32Array ||
-    o instanceof Float64Array ||
-    o instanceof BigInt64Array ||
-    o instanceof BigUint64Array
-  ) {
-    return o.map((e) => deepCopy(e));
-  }
-
-  // Object
-  const finalObject = {};
-  for (const k in o) {
-    if (o.hasOwnProperty(k)) {
-      finalObject[k] = deepCopy(o[k]);
-    }
-  }
-  return finalObject;
+export function deepCopy<Type>(value: Type): Type {
+  return deepmerge({}, value);
 }
